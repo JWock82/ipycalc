0729e4cca47616a94dff58030c7d3934ad583d99
Craig <33323539+JWock82@users.noreply.github.com>
2022-10-14 08:04:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+
+    install_requires=[
+        'IPython',
+    ],
+
     entry_points={
         'nbconvert.exporters': [
             'ipycalc = ipycalc.exporter:ipycalcExporter',
