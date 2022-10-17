7d327e8adaf01354d6418a1863ff1ac5fe07ac18
Craig Brinck <cbrinck@outlook.com>
2022-10-17 13:14:30 -0400

    Whitespace changes

--- setup.py
+++ setup.py
@@ -12,22 +12,17 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JWock82/ipycalc.git",
-    
     packages=setuptools.find_packages(include=['ipycalc', 'ipycalc.*']),
-    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-
     install_requires=[
         'jupyterlab',
         'nbconvert>=6.0',
     ],
-
     include_package_data=True,
-
     entry_points={
         'nbconvert.exporters': [
             'ipycalc = ipycalc:ipycalcExporter',
