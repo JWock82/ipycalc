793718ba84b1ce0be61e4b5f22227d414fa2a6c2
Craig Brinck <cbrinck@outlook.com>
2023-07-12 09:02:20 -0400

    Updated nbconvert exporter

--- ipycalc/__init__.py
+++ ipycalc/__init__.py
@@ -10,12 +10,12 @@ class ipycalcExporter(WebPDFExporter):
     export_from_notebook = 'ipycalc via webpdf'
 
     @property
-    def _template_paths(self):
-        return super()._template_paths + [os.path.join(os.path.dirname(__file__), "nbconvert_templates")]
+    def template_paths(self):
+        return super().template_paths + [os.path.join(os.path.dirname(__file__), "nbconvert_templates")]
     
     def _template_file_default(self):
         """
         We want to use the new template we ship with our library.
         """
-        return 'ipycalc'
+        return 'ipycalc.tpl'
     
