ba6939603025503abd2d7e1e1055d5361bd38113
Craig Brinck <cbrinck@outlook.com>
2023-07-12 09:46:16 -0400

    Update nbconvert template

--- ipycalc/__init__.py
+++ ipycalc/__init__.py
@@ -9,17 +9,13 @@ class ipycalcExporter(WebPDFExporter):
 
     export_from_notebook = 'ipycalc via webpdf'
 
-    def _template_name_default(self):
-        return 'ipycalc'
-
-    @property
-    def _template_paths(self):
-        return super()._template_paths + [os.path.join(os.path.dirname(__file__), "nbconvert_templates")]
-    
-    def _default_extra_template_basedirs(self):
-        return [os.path.join(os.path.dirname(__file__), 'nbconvert_templates')]
-
     def _template_file_default(self):
         template_dir = os.path.join(os.path.dirname(__file__), 'nbconvert_templates')
         return os.path.join(template_dir, 'ipycalc.tpl')
+
+    @property
+    def template_file(self):
+        if not hasattr(self, '_template_file'):
+            self._template_file = self._template_file_default()
+        return self._template_file
     
